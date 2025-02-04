7d86b545df8a164cc7007448aab64d8b58c0209d
latronixo <pamerra@mail.ru>
2025-02-04 12:10:08 +0300

    Initial Commit

---
+++ 229. Вымя/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  229. Вымя
+//
+//  Created by Валентин Картошкин on 04.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
